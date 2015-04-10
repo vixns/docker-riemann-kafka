072bb3da25a6e65628f0d091806ab0c5cfcb07eb
Stéphane Cottin <stephane.cottin@vixns.com>
2015-04-10 20:19:44 +0200

    remove elasticsearch jar, upgrade to 0.2.9

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,4 @@
 FROM vixns/riemann
 MAINTAINER Stéphane Cottin <stephane.cottin@vixns.com>
-
 COPY riemann-kafka.jar /usr/share/riemann/riemann-kafka.jar
-COPY riemann-elasticsearch-output-0.1.1-SNAPSHOT-standalone.jar /usr/share/riemann/riemann-elasticsearch-output-0.1.1-SNAPSHOT-standalone.jar
-
 COPY riemann.sh /usr/local/bin/riemann.sh
-
-ENV KAFKA_TOPIC logs
-ENV KAFKA_GROUP riemann.group
--- riemann.sh
+++ riemann.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 exec ${JAVA_HOME}/bin/java -server -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSParallelRemarkEnabled \
 -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -XX:+UseCompressedOops -XX:+CMSClassUnloadingEnabled \
--cp "/usr/share/riemann/riemann-kafka.jar:/usr/share/riemann/riemann.jar:/usr/share/riemann/riemann-elasticsearch-output-0.1.1-SNAPSHOT-standalone.jar" \
+-cp "/usr/share/riemann/riemann-kafka.jar:/usr/share/riemann/riemann.jar" \
 riemann.bin start /etc/riemann/riemann.config
