48a335113eeee3a07a29722b18ff986f6a7a29de
Stéphane Cottin <stephane.cottin@vixns.com>
2015-08-16 14:48:59 +0200

    add elasticsearch output plugin, because rivers are deprecated

--- riemann.sh
+++ riemann.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 exec ${JAVA_HOME}/bin/java -server -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSParallelRemarkEnabled \
 -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -XX:+UseCompressedOops -XX:+CMSClassUnloadingEnabled \
--cp "/usr/share/riemann/riemann-kafka.jar:/usr/share/riemann/riemann.jar" \
+-cp "/usr/share/riemann/riemann.jar:/usr/share/riemann/riemann-kafka.jar:/usr/share/riemann/elastic-riemann-0.1.1-SNAPSHOT-standalone.jar" \
 riemann.bin start /etc/riemann/riemann.config
