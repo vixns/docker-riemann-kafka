93f83d03eba249b31f5246cb2679f37db6a3fad4
Stéphane Cottin <stephane.cottin@vixns.com>
2016-12-02 00:23:56 +0100

    classpath fix

--- riemann.sh
+++ riemann.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-export EXTRA_CLASSPATH=/usr/share/riemann/riemann-kafka.jar:/usr/share/riemann/riemann-elasticsearch-output.jar
 exec ${JAVA_HOME}/bin/java -server -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSParallelRemarkEnabled \
 -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -XX:+UseCompressedOops -XX:+CMSClassUnloadingEnabled \
--cp "/usr/share/riemann/riemann.jar" \
+-cp "/usr/share/riemann/riemann.jar:/usr/share/riemann/riemann-kafka.jar:/usr/share/riemann/riemann-elasticsearch-output.jar" \
 riemann.bin start /etc/riemann/riemann.config
